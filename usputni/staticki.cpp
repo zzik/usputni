9be552a74ff6dceb9b585b792c7572420d08f9bb
zzik <edin.bashich@gmail.com>
2025-01-24 11:22:09 +0100

    cmon bby

---
+++ usputni/staticki.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+
+}
+
+//napravi 2 2d niza
+//saberi ih u treci niz
+//vodoravno slozit u treci
+//vertikalno slozit u cetvrti
+//
+//uradi preko dinamicke i staticke memorije
+//
+//rendomizuj vrijednosti varijabli
+//
+//dodatni zadataci
+//1. redoslijed brojeva neparan, paran
+//2. red parnih, red neparnih
+//3. izracunat prosjek svih i pojedinacnih redova, isto za brojeva, i matrica
+//4. indeks reda sa najvisim prosjekom
